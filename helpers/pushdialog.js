5b970b41b6bf2f6e6adcb657b1bec313a690f476
masetio <mas.etio@gmail.com>
2021-12-25 19:08:40 +0700

    Update pushdialog.js

--- helpers/pushdialog.js
+++ helpers/pushdialog.js
@@ -2,9 +2,9 @@ const axios = require('axios');
 
 async function makeGetRequest() {
 
-    let payload = { queryText:'assalamu'alaikum', sessionId: 'abs2342cd1234', languageCode:'id' };
+    const  payload = { queryText:'assalamu'alaikum', sessionId: 'abs2342cd1234', languageCode:'id' };
 
-    let res = await axios.post('https://621f-13-229-66-36.ngrok.io/dialogflow', payload);
+    const  res = await axios.post('https://621f-13-229-66-36.ngrok.io/dialogflow', payload);
 
     let data = res.data;
     return data;
